942b853177f4ba61266aefdce2ba7512dd121e29
Alice Chan <alicebay10@yahoo.co.uk>
2024-12-26 17:52:49 +0000

    created an app 'likes', and added create function to LikeSerializer that avoid users from liking the same post twice.

--- posts/tests.py
+++ posts/tests.py
@@ -26,8 +26,8 @@ class PostListViewTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     #def test_user_not_logged_in_cant_create_post(self):
-    #    response = self.client.post('/posts/', {'title': 'a title'})
-    #    self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        #response = self.client.post('/posts/', {'title': 'a title'})
+        #self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 class PostDetailViewTests(APITestCase):
     def setUp(self):
