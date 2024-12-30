29c02bc55d2efe553448257b3304e5e8af5167cb
Alice Chan <alicebay10@yahoo.co.uk>
2024-12-30 12:40:19 +0000

    updated the allowed host name.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'drf-api-alice.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'drf-api-alice-c6521fcf7abe.herokuapp.com']
 
 
 # Application definition
